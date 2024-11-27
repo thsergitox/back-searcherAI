d38bfa8d1901e59dfa8ea9956b54e993e88e15c5
thsergitox <sergiopezoj@gmail.com>
2024-11-26 22:51:51 -0500

    feat: add CORS middleware configuration to FastAPI application

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import router
 app = FastAPI()
 
@@ -7,5 +8,16 @@ app = FastAPI(
     description="Multi Agetn System for Knowledge Graph Construction",
     version="0.1"
 )
+
+
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite cualquier origen
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos
+    allow_headers=["*"],  # Permite todos los headers
+)
+
 
 app.include_router(router, prefix="/api")
